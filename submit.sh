be73a77dca4991b290ac7802cb2565c0cc4b63b1
David Roundy <roundyd@physics.oregonstate.edu>
2015-08-07 12:39:57 -0400

    non-working submit script

---
+++ submit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export API_TOKEN=FtpwGAy9ndcLXLUlH7i96rgXLgi2SzEdym2caXEsNUI
+export TEAM_ID=97
+export OUTPUT='@output.json'
+
+echo curl --user :$API_TOKEN -X POST -H "Content-Type: application/json" \
+        -d $OUTPUT \
+        https://davar.icfpcontest.org/teams/$TEAM_ID/solutions
+curl --user :$API_TOKEN -X POST -H "Content-Type: application/json" \
+        -d $OUTPUT \
+        https://davar.icfpcontest.org/teams/$TEAM_ID/solutions
