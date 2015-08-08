2a557de413a09aaede09dc474f63d6cf764971f5
vischerb <br.vischer@gmail.com>
2015-08-08 15:32:51 -0700

    Parsing.

--- get.sh
+++ get.sh
@@ -4,5 +4,11 @@ export API_TOKEN=FtpwGAy9ndcLXLUlH7i96rgXLgi2SzEdym2caXEsNUI=
 export TEAM_ID=97
 export OUTPUT='@output.json'
 
-curl --user :$API_TOKEN -X GET https://davar.icfpcontest.org/teams/$TEAM_ID/solutions
+echo rm metadata.json
+rm metadata.json
+
+curl --user :$API_TOKEN -X GET https://davar.icfpcontest.org/teams/$TEAM_ID/solutions > metadata.json
+
+echo python parse.py
+python parse.py
 
---
+++ parse.py
@@ -0,0 +1,30 @@
+#usr/bin/pthon
+import os
+
+splitLen = 200         # 200 lines per file
+outputBase = 'parsed/output' # output.1.txt, output.2.txt, etc.
+
+# This is shorthand and not friendly with memory
+# on very large files (Sean Cavanagh), but it works.
+input = open('metadata.json', 'r').read().split('}')
+
+#NOTE: Parses forwards, look at the highest file number for most recent
+if os.path.exists('parsed'):
+    os.system("rm -r parsed")
+os.system("mkdir -p parsed")
+
+at = 1
+for lines in range(0, len(input), splitLen):
+    # If file doesn't exist or if last 
+    # First, get the list slice
+    outputData = input[lines:lines+splitLen]
+    
+    # Now open the output file, join the new slice with newlines
+    # and write it out. Then close the file.
+    output = open(outputBase + str(at) + '.dat', 'w')
+    output.write('\n\n'.join(outputData))
+    output.close()
+
+    # Increment the counter
+    at += 1
+        
