8c552c81d1ee9d129e2d443bafff79e1737c58ff
David Roundy <roundyd@physics.oregonstate.edu>
2015-08-09 08:37:08 -0400

    fix bug in phrase-handling in run-problems script.

--- run-problems.sh
+++ run-problems.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-PHRASES=-p 'ei!' -p 'Ia! Ia!' -p "R'lyeh" -p Yuggoth
-
 cargo build --release
 
 for x in problems/*.json; do
-    srun -J $x target/release/solve-davar -f $x $PHRASES --solver mc -t 360 -c 1 --submit --save &
+    srun -J $x target/release/solve-davar -f $x \
+         -p 'ei!' -p 'Ia! Ia!' -p "R'lyeh" -p Yuggoth \
+         --solver mc -t 360 -c 1 --submit --save &
 done
