d3707ea745936cebc088b9aa97bdd0a244a687ae
David Roundy <roundyd@physics.oregonstate.edu>
2015-08-09 08:36:11 -0400

    build prior to submitting

--- run-problems.sh
+++ run-problems.sh
@@ -2,6 +2,8 @@
 
 PHRASES=-p 'ei!' -p 'Ia! Ia!' -p "R'lyeh" -p Yuggoth
 
+cargo build --release
+
 for x in problems/*.json; do
     srun -J $x target/release/solve-davar -f $x $PHRASES --solver mc -t 360 -c 1 --submit --save &
 done
