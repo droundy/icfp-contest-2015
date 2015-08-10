409ccb3bb22bd2fe3fc2bfa34c480101e9d9e4c8
David Roundy <roundyd@physics.oregonstate.edu>
2015-08-10 12:13:18 -0400

    update record-screen.sh script

--- record-screen.sh
+++ record-screen.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo $1
-TIME=300
+TIME=30
 
 test -e problems/problem_$1.json
 
@@ -16,8 +16,8 @@ done
 echo \$ echo Solving problem_$1.json in $TIME seconds.
 echo Running problem_$1.json in $TIME seconds.
 
-echo \$ ./play_icfp2015 -p 'ei!' -p "'Ia! Ia!'" -p "R'lyeh" -p Yuggoth -p yogsothoth -p "'planet 10'" -p "'john bigboote'" -t $TIME -c 2 --animate 250 -f problems/problem_$1.json &
+echo \$ ./play_icfp2015 -p 'ei!' -p "'Ia! Ia!'" -p "R'lyeh" -p Yuggoth -p yogsothoth -p "'planet 10'" -p "'john bigboote'" -t $TIME -c 2 --animate 40 -f problems/problem_$1.json &
 
 ./play_icfp2015 -p 'ei!' -p 'Ia! Ia!' -p "R'lyeh" -p Yuggoth -p yogsothoth -p 'planet 10' -p 'john bigboote' -t $TIME -c 1 -f problems/problem_$1.json > /dev/null
 
-ffmpeg -f x11grab -show_region 1 -y -r 25 -s 1280x720 -i :0.0+0,40 -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis screencast_$1.webm 2> /dev/null
+ffmpeg -f x11grab -show_region 1 -y -r 25 -s 1280x720 -i :0.0+0,40 -vcodec ffv1 screencast_$1.avi 2> /dev/null
