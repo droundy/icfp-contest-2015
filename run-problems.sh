51fc54d7a8bf4b4303540845ac49e531a0746ef8
Paho Lurie-Gregg <paho@knightley.physics.oregonstate.edu>
2015-08-10 04:39:40 -0700

    updates

--- run-problems.sh
+++ run-problems.sh
@@ -3,5 +3,5 @@
 cargo build --release
 
 for x in problems/*.json; do
-    srun -J $x ./run.py -f $x -t 1800 --solver bottomupdfs --save &
+    srun -J $x ./run.py -f $x -t 1300 --solver bottomupdfs --save &
 done
