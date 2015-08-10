0f0a8aeb9104cff94f33b786e708e0abf91d8f2e
elliotc12 <elliotc12@gmail.com>
2015-08-10 01:18:32 -0700

    fix submit_best.py

--- submit_best.py
+++ submit_best.py
@@ -22,11 +22,12 @@ for n in range(0,25):
     for i in files:
         if int(i[0]) == n:
             my_list.append(i)
-    #print sorted(my_list, key=lambda item: item[2], reverse=True)[0]
     out_json_files.append( sorted(my_list, key=lambda item: item[2], reverse=True)[0] )
 
 for e in out_json_files:
-    fname = e[0] + "-" + e[1] + "-" + e[2] + ".json"
-    post_cmd = "curl --user :" + str(API_TOKEN) + " -X POST -H \"Content-Type: application/json " + \
+    fname = '@solutions/' + e[0] + "-" + e[1] + "-" + e[2] + ".json"
+    post_cmd = "curl --user :" + str(API_TOKEN) + " -X POST -H \"Content-Type: application/json\" " + \
             "-d " + fname + " https://davar.icfpcontest.org/teams/" + str(TEAM_ID) + "/solutions"
     print post_cmd
+    split_cmd = shlex.split(post_cmd)
+    subprocess.Popen(split_cmd)
